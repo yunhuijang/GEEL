Add molecule model - Fix data_utils that can group molecules (Br, Cl token) - Add QM9 / ZINC dataset to dataset - Add mol_uttils - Add tokens for molecule data - Add NSPDK calculation to evaluation - Add sampling molecules to train_generator

data/data_utils.py
@@ -9,13 +9,15 @@ from treelib import Tree, Node
 from sklearn.model_selection import train_test_split
 from itertools import zip_longest
 import networkx as nx
-import pickle
 
-# from data.dataset import DATASETS
-# from data.orderings import ORDER_FUNCS, order_graphs
+from data.tokens import grouper_mol
 
 
 DATA_DIR = "resource"
+NODE_TYPE_DICT = {'F': 9, 'O': 10, 'N': 11, 'C': 12, 'P': 13, 'I': 14, 'Cl': 15, 'Br': 16, 'S': 17}
+TYPE_NODE_DICT = {str(key): value for value, key in NODE_TYPE_DICT.items()}
+BOND_TYPE_DICT = {1: 5, 2: 6, 3: 7, 1.5: 8}
+TYPE_BOND_DICT = {key: value for value, key in NODE_TYPE_DICT.items()}
 
 def get_level(node):
     return int(node.identifier.split('-')[0])
@@ -124,7 +126,7 @@ def adj_to_k2_tree(adj, return_tree=False, is_wholetree=False, k=4, is_mol=False
 
 def check_tree_validity(tree):
     depth = tree.depth()
-    leaves = [leaf for leaf in tree.leaves() if leaf.tag == 1]
+    leaves = [leaf for leaf in tree.leaves() if leaf.tag != '0']
     invalid_leaves = [leaf for leaf in leaves if tree.depth(leaf)!=depth]
     if len(invalid_leaves) == 0:
         return True
@@ -138,30 +140,18 @@ def tree_to_adj(tree):
     tree = map_starting_point(tree)
     k = get_k(tree)
     depth = tree.depth()
-    leaves = [leaf for leaf in tree.leaves() if leaf.tag == 1]
+    leaves = [leaf for leaf in tree.leaves() if leaf.tag != '0']
     one_data_points = [leaf.data for leaf in leaves]
     x_list = [data[0] for data in one_data_points]
     y_list = [data[1] for data in one_data_points]
+    label_list = [NODE_TYPE_DICT[leaf.tag] if leaf.tag in NODE_TYPE_DICT.keys() else int(leaf.tag) for leaf in leaves]
     matrix_size = int(k**depth)
     adj = torch.zeros((matrix_size, matrix_size))
-    adj[x_list, y_list] = 1
+    for x, y, label in zip(x_list, y_list, label_list):
+        adj[x, y] = label
     
     return adj
 
-def map_starting_point_node(node, tree):
-    # TODO: fix error (None: node update 못하는 것 고치기)
-    print(node)
-    parent = get_parent(node, tree)
-    siblings = get_children_identifier(parent, tree)
-    index = siblings.index(node.identifier)
-    level = get_level(node)
-    tree_depth = tree.depth()
-    k = get_k(tree)
-    matrix_size = k**tree_depth
-    adding_value = int(matrix_size/(k**level))
-    parent_starting_point = parent.data
-    node.data = (parent_starting_point[0]+adding_value*int(index/2), parent_starting_point[1]+adding_value*int(index%2))
-    return node.data
 
 def map_starting_point(tree):
     '''
@@ -170,10 +160,6 @@ def map_starting_point(tree):
     bfs_list = [tree[node] for node in tree.expand_tree(mode=Tree.WIDTH, 
                                                             key=lambda x: (int(x.identifier.split('-')[0]), int(x.identifier.split('-')[1])),)]
     bfs_list[0].data = (0,0)
-    # start_time = time()
-    # start_points = Parallel(n_jobs=4, backend='multiprocessing')(delayed(map_starting_point_node)(node, tree) for node in bfs_list[1:])
-    
-    # print(f'elapsed change: {start_time - time()}')
     
     for node in bfs_list[1:]:
         parent = get_parent(node, tree)
@@ -290,22 +276,26 @@ def grouper(n, iterable, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
 
-def bfs_string_to_tree(string):
+def bfs_string_to_tree(string, is_zinc=False):
     tree = Tree()
     tree.create_node("root", "0")
     parent_node = tree["0"]
     node_deque = deque([])
-    for node_1, node_2, node_3, node_4 in grouper(4, string):
+    if is_zinc:
+        node_groups = grouper_mol(string)
+    else:
+        node_groups = grouper(4, string)
+    for node_1, node_2, node_3, node_4 in node_groups:
         parent_level = get_level(parent_node)
         cur_level_max = max([get_location(node) for node in tree.nodes.values() if get_level(node) == parent_level+1], default=0)
         for i, node_tag in enumerate([node_1, node_2, node_3, node_4], 1):
             if node_tag == None:
                 break
-            new_node = Node(tag=int(node_tag), identifier=f"{parent_level+1}-{cur_level_max+i}")
+            new_node = Node(tag=node_tag, identifier=f"{parent_level+1}-{cur_level_max+i}")
             tree.add_node(new_node, parent=parent_node)
             node_deque.append(new_node)
         parent_node = node_deque.popleft()
-        while(parent_node.tag == 0):
+        while(parent_node.tag == '0'):
             if len(node_deque) == 0:
                 return tree
             parent_node = node_deque.popleft()
@@ -313,7 +303,6 @@ def bfs_string_to_tree(string):
 
 def clean_string(string):
 
-    
     if "[pad]" in string:
         string = string[:string.index("[pad]")]
         

data/dataset.py
@@ -1,8 +1,6 @@
 import torch
 from torch.utils.data import Dataset
-from torch.nn.utils.rnn import pad_sequence
-from joblib import Parallel, delayed
-from tqdm import tqdm
+from pathlib import Path
 import pickle
 import os
 from collections import deque
@@ -23,84 +21,6 @@ DATASETS = {  # name: (graphs, num_repetitions)
     "grid_small": (get_grid_small_data, 1),
     "Caveman": (get_caveman_data, 1)
 }
-
-# class K2TreeListDataset(Dataset):
-#     def __init__(self, ordered_graphs: list):
-#         super().__init__()
-#         self.tree_leaf_lists = Parallel(n_jobs=8)(delayed(adj_to_k2_tree)(graph.to_adjacency()) 
-#                                                   for graph in tqdm(ordered_graphs, "Converting adj into tree"))
-#         self.tree_lists = [tree_leaf[0] for tree_leaf in self.tree_leaf_lists]
-#         self.leaf_lists = [tree_leaf[1] for tree_leaf in self.tree_leaf_lists]
-#         self.tree_leaf_tensors = [torch.stack(tree_leaf[0]+tree_leaf[1]) for tree_leaf in self.tree_leaf_lists]
-    
-#     def __len__(self):
-#         return len(self.tree_lists)
-    
-#     def __getitem__(self, idx: int):
-        
-#         return self.tree_leaf_tensors[idx]
-        
-#     def collate_fn(batch):
-#         data = []
-#         for tree_leaf in batch:
-#             data.append(tree_leaf)
-#         return pad_sequence(data, batch_first=True, padding_value=0)
-    
-# class K2TreeDataset(Dataset):
-#     def __init__(self, ordered_graphs: list, string_type='dfs'):
-#         super().__init__()
-#         self.string_type = string_type
-#         self.trees = Parallel(n_jobs=8)(delayed(adj_to_k2_tree)(graph.to_adjacency(), return_tree=True) 
-#                                         for graph in tqdm(ordered_graphs, "Converting adj into tree"))
-#         if string_type == 'dfs':
-#             self.strings = Parallel(n_jobs=8)(delayed(tree_to_dfs_string)(tree) 
-#                                           for tree in tqdm(self.trees, "Converting tree into string"))
-#         elif string_type in ['bfs', 'group', 'bfs-deg', 'bfs-tri', 'bfs-deg-group']:
-#             self.strings = Parallel(n_jobs=8)(delayed(tree_to_bfs_string)(tree, string_type) 
-#                                           for tree in tqdm(self.trees, "Converting tree into string"))
-#         self.max_depth = max([tree.depth() for tree in self.trees])
-        
-#     def __len__(self):
-#         return len(self.trees)
-    
-#     def __getitem__(self, idx: int):
-#         return torch.LongTensor(tokenize(self.strings[idx], self.string_type))
-    
-#     def collate_fn(batch):
-#         data = []
-#         for string in batch:
-#             data.append(string)
-
-#         return pad_sequence(data, batch_first=True, padding_value=0)
-    
-# class K2StringTreeDataset(Dataset):
-#     def __init__(self, ordered_graphs: list, string_type='dfs'):
-#         super().__init__()
-#         self.string_type = string_type
-#         self.org_trees = Parallel(n_jobs=8)(delayed(adj_to_k2_tree)(graph.to_adjacency(), return_tree=True) 
-#                                         for graph in tqdm(ordered_graphs, "Converting adj into tree"))
-#         self.trees = Parallel(n_jobs=8)(delayed(map_tree_pe)(tree) for tree in tqdm(self.org_trees, "Map tree PE"))
-#         if string_type == 'dfs':
-#             self.strings = Parallel(n_jobs=8)(delayed(tree_to_dfs_string)(tree) 
-#                                           for tree in tqdm(self.trees, "Converting tree into string"))
-#         elif string_type in ['bfs', 'group', 'bfs-deg', 'bfs-tri', 'bfs-deg-group']:
-#             self.strings = Parallel(n_jobs=8)(delayed(tree_to_bfs_string)(tree, string_type) 
-#                                           for tree in tqdm(self.trees, "Converting tree into string"))
-#         self.max_depth = max([tree.depth() for tree in self.trees])
-        
-#     def __len__(self):
-#         return len(self.org_trees)
-    
-#     def __getitem__(self, idx: int):
-#         return torch.LongTensor(tokenize(self.strings[idx], self.string_type)), self.trees[idx]
-    
-#     def collate_fn(batch):
-#         data = []
-#         tree_list = []
-#         for string, tree in batch:
-#             data.append(string)
-#             tree_list.append(tree)
-#         return pad_sequence(data, batch_first=True, padding_value=0), tree_list
     
 class EgoDataset(Dataset):
     data_name = "ego_small"
@@ -111,9 +31,9 @@ class EgoDataset(Dataset):
         string_path = os.path.join(self.raw_dir, f"{self.data_name}_str_{split}.pkl")
         with open(string_path, 'rb') as f:
             self.strings = pickle.load(f)
-            if self.string_type in ['bfs-deg', 'bfs-deg-group']:
-                self.strings = [self.map_deg_string(string) for string in self.strings]
-        
+        if self.string_type in ['bfs-deg', 'bfs-deg-group']:
+            self.strings = [self.map_deg_string(string) for string in self.strings]
+    
     def __len__(self):
         return len(self.strings)
     
@@ -150,3 +70,16 @@ class GridDataset(EgoDataset):
 class GridSmallDataset(EgoDataset):
     data_name = 'grid_small'
     raw_dir = f'{DATA_DIR}/grid_small'
+
+class QM9Dataset(EgoDataset):
+    data_name = "qm9"
+    raw_dir = f"{DATA_DIR}/qm9"
+    def __init__(self, split, string_type='bfs', is_tree=False):
+        self.string_type = string_type
+        self.is_tree = is_tree
+        string_path = os.path.join(self.raw_dir, f"{self.data_name}_str_{split}.txt")
+        self.strings = Path(string_path).read_text(encoding="utf=8").splitlines()
+        
+class ZINCDataset(QM9Dataset):
+    data_name = 'zinc'
+    raw_dir = f'{DATA_DIR}/zinc'

data/orderings.py
@@ -157,7 +157,7 @@ class OrderedGraph:
 def order_graphs(
     graphs: list,
     order_func,
-    num_repetitions: int = 1, seed: int = 0
+    num_repetitions: int = 1, seed: int = 0, is_mol=False
 ):
     ordered_graphs = []
     for i, graph in enumerate(graphs):
@@ -165,7 +165,8 @@ def order_graphs(
             # seed = i * (j + 1) + j
             random.seed(seed)
             np.random.seed(seed)
-            graph.remove_edges_from(nx.selfloop_edges(graph))
+            if not is_mol:
+                graph.remove_edges_from(nx.selfloop_edges(graph))
             graph = nx.convert_node_labels_to_integers(graph)
             order, bw = order_func(graph, seed)
             ordered_graphs.append(OrderedGraph(

evaluation/evaluation.py
@@ -12,6 +12,8 @@ import networkx as nx
 from functools import partial
 import random
 import subprocess as sp
+from eden.graph import vectorize
+from sklearn.metrics.pairwise import pairwise_kernels
 
 from data.tokens import TOKENS_DICT
 
@@ -343,11 +345,37 @@ def orbit_stats_all(graph_ref_list, graph_pred_list, KERNEL=gaussian):
         print('Time computing orbit mmd: ', elapsed)
     return mmd_dist
 
+### code adapted from https://github.com/idea-iitd/graphgen/blob/master/metrics/mmd.py
+def compute_nspdk_mmd(samples1, samples2, metric, is_hist=True, n_jobs=None):
+    def kernel_compute(X, Y=None, is_hist=True, metric='linear', n_jobs=None):
+        X = vectorize(X, complexity=4, discrete=True)
+        if Y is not None:
+            Y = vectorize(Y, complexity=4, discrete=True)
+        return pairwise_kernels(X, Y, metric='linear', n_jobs=n_jobs)
+
+    X = kernel_compute(samples1, is_hist=is_hist, metric=metric, n_jobs=n_jobs)
+    Y = kernel_compute(samples2, is_hist=is_hist, metric=metric, n_jobs=n_jobs)
+    Z = kernel_compute(samples1, Y=samples2, is_hist=is_hist, metric=metric, n_jobs=n_jobs)
+
+    return np.average(X) + np.average(Y) - 2 * np.average(Z)
+
+##### code adapted from https://github.com/idea-iitd/graphgen/blob/master/metrics/stats.py
+def nspdk_stats(graph_ref_list, graph_pred_list):
+    graph_pred_list_remove_empty = [G for G in graph_pred_list if not G.number_of_nodes() == 0]
+
+    prev = datetime.now()
+    mmd_dist = compute_nspdk_mmd(graph_ref_list, graph_pred_list_remove_empty, metric='nspdk', is_hist=False, n_jobs=20)
+    elapsed = datetime.now() - prev
+    if PRINT_TIME:
+        print('Time computing degree mmd: ', elapsed)
+    return mmd_dist
+
 METHOD_NAME_TO_FUNC = {
     'degree': degree_stats,
     'cluster': clustering_stats,
     'orbit': orbit_stats_all,
-    'spectral': spectral_stats
+    'spectral': spectral_stats,
+    'nspdk': nspdk_stats
 }
 
 
@@ -357,6 +385,9 @@ def eval_graph_list(graph_ref_list, graph_pred_list, methods=None, kernels=None)
         methods = ['degree', 'cluster', 'orbit']
     results = {}
     for method in methods:
-        results[method] = round(METHOD_NAME_TO_FUNC[method](graph_ref_list, graph_pred_list, kernels[method]), 6)
+        if method == 'nspdk':
+            results[method] = METHOD_NAME_TO_FUNC[method](graph_ref_list, graph_pred_list)
+        else:
+            results[method] = round(METHOD_NAME_TO_FUNC[method](graph_ref_list, graph_pred_list, kernels[method]), 6)
         print('\033[91m' + f'{method:9s}' + '\033[0m' + ' : ' + '\033[94m' +  f'{results[method]:.6f}' + '\033[0m')
     return results

model/trans_generator.py
@@ -51,7 +51,8 @@ class TreePositionalEncoding(nn.Module):
             self.max_pe_length = int(math.log(max_len, 4)+5)*4
         self.positional_embedding = nn.Linear(self.max_pe_length, self.d_model)
         self.padder = torch.nn.ReplicationPad2d((0,self.d_model-self.max_pe_length,0,0))
-        is_group_dict = {'emb': False, 'group-emb': True, 'pad': False, 'group-pad': True}
+        is_group_dict = {'emb': False, 'group-emb': True, 'pad': False, 'group-pad': True,
+                         'qm9': True, 'zinc': True}
         self.is_group = is_group_dict[self.pos_type]
         
     def filter_row_string(self, row_string):

trainer/train_generator.py
@@ -7,14 +7,18 @@ from pytorch_lightning.loggers import WandbLogger
 import wandb
 from time import gmtime, strftime
 import pickle
+import os
+from moses.metrics.metrics import get_all_metrics
 
 from data.dataset import EgoDataset, ComDataset, EnzDataset, GridDataset, GridSmallDataset, QM9Dataset, ZINCDataset
-from data.data_utils import dfs_string_to_tree, tree_to_adj, check_validity, train_val_test_split, bfs_string_to_tree, adj_to_graph, check_tree_validity
+from data.data_utils import dfs_string_to_tree, tree_to_adj, check_validity, bfs_string_to_tree, adj_to_graph, check_tree_validity
+from data.mol_utils import adj_to_graph_mol, mols_to_smiles, check_adj_validity_mol, mols_to_nx
 from evaluation.evaluation import compute_sequence_accuracy, compute_sequence_cross_entropy, save_graph_list, load_eval_settings, eval_graph_list
 from plot import plot_graphs_list
 from model.lstm_generator import LSTMGenerator
 from data.tokens import untokenize
 
+directory = 'resource'
 
 class BaseGeneratorLightningModule(pl.LightningModule):
     def __init__(self, hparams):
@@ -37,11 +41,12 @@ class BaseGeneratorLightningModule(pl.LightningModule):
             'qm9': QM9Dataset,
             'zinc': ZINCDataset
         }.get(hparams.dataset_name)
-        try:
-            with open(f'resource/{hparams.dataset_name}/{hparams.dataset_name}' + f'_test_graphs.pkl', 'rb') as f:
-                self.test_graphs = pickle.load(f)
-        except:
-            with open(f'gcg/resource/{hparams.dataset_name}/{hparams.dataset_name}' + f'_test_graphs.pkl', 'rb') as f:
+        if hparams.dataset_name in ['qm9', 'zinc']:
+            with open(f'{directory}/{hparams.dataset_name}/{hparams.dataset_name}' + f'_smiles_train.txt', 'r') as f:
+                self.train_smiles = f.readlines()
+            with open(f'{directory}/{hparams.dataset_name}/{hparams.dataset_name}' + f'_smiles_test.txt', 'r') as f:
+                self.test_smiles = f.readlines()
+            with open(f'{directory}/{hparams.dataset_name}/{hparams.dataset_name}' + f'_test_graphs.pkl', 'rb') as f:
                 self.test_graphs = pickle.load(f)
         self.train_dataset, self.val_dataset, self.test_dataset = [dataset_cls(split, self.string_type, is_tree=(not hparams.tree_pos))
                                                                    for split in ['train', 'val', 'test']]
@@ -99,9 +104,12 @@ class BaseGeneratorLightningModule(pl.LightningModule):
             if self.hparams.string_type == 'dfs':
                 valid_string_list = [string for string in string_list if check_validity(string)]
                 sampled_trees = [dfs_string_to_tree(string) for string in valid_string_list]
-            elif self.hparams.string_type in ['bfs', 'group', 'bfs-deg', 'bfs-deg-group']:
+            elif self.hparams.string_type in ['bfs', 'group', 'bfs-deg', 'bfs-deg-group', 'qm9', 'zinc']:
                 valid_string_list = [string for string in string_list if len(string)>0 and len(string)%4 == 0]
-                sampled_trees = [bfs_string_to_tree(string) 
+                is_zinc = False
+                if self.hparams.string_type == 'zinc':
+                    is_zinc = True
+                sampled_trees = [bfs_string_to_tree(string, is_zinc) 
                                 for string in tqdm(valid_string_list, "Sampling: converting string to tree")]
                 
                 
@@ -112,22 +120,45 @@ class BaseGeneratorLightningModule(pl.LightningModule):
                 table.add_data(org_string, string, (len(string)>0 and len(string)%4 == 0))
             wandb.log({'strings': table})
             valid_sampled_trees = [tree for tree in sampled_trees if (tree.depth() <= self.max_depth) and check_tree_validity(tree)]
-            wandb.log({"tree-validity": len(valid_sampled_trees) / len(sampled_trees)})
-            valid_sampled_trees = valid_sampled_trees[:len(self.test_graphs)]
-            sampled_graphs = [adj_to_graph(tree_to_adj(tree).numpy()) 
-                              for tree in tqdm(valid_sampled_trees, "Sampling: converting tree into graph")]
-            save_graph_list(self.hparams.dataset_name, self.ts, sampled_graphs, valid_string_list, string_list, org_string_list)
-            plot_dir = f'{self.hparams.dataset_name}/{self.ts}'
-            plot_graphs_list(sampled_graphs, save_dir=plot_dir)
-            wandb.log({"samples": wandb.Image(f'./samples/fig/{plot_dir}/title.png')})
-
-            # GDSS evaluation
-            methods, kernels = load_eval_settings('')
-            if len(sampled_graphs) == 0:
-                mmd_results = {'degree': np.nan, 'orbit': np.nan, 'cluster': np.nan, 'spectral': np.nan}
+            if self.hparams.string_type in ['zinc', 'qm9']:
+                # valid_sampled_trees = sampled_trees[:len(self.test_graphs)]
+                adjs = [tree_to_adj(tree).numpy() for tree in tqdm(valid_sampled_trees, "Sampling: converting tree into adj")]
+                valid_adjs = [valid_adj for valid_adj in [check_adj_validity_mol(adj) for adj in adjs] if valid_adj is not None]
+                mols_no_correct = [adj_to_graph_mol(adj) for adj in valid_adjs]
+                mols_no_correct = [elem for elem in mols_no_correct if elem[0] is not None]
+                mols = [elem[0] for elem in mols_no_correct]
+                no_corrects = [elem[1] for elem in mols_no_correct]
+                num_mols = len(mols)
+                gen_smiles = mols_to_smiles(mols)
+                gen_smiles = [smi for smi in gen_smiles if len(smi)]
+                save_dir = f'{self.hparams.dataset_name}/{self.ts}'
+                with open(f'samples/smiles/{save_dir}.txt', 'w') as f:
+                    for smiles in gen_smiles:
+                        f.write(f'{smiles}\n')
+                scores = get_all_metrics(gen=gen_smiles, device=self.device, n_jobs=8, test=self.test_smiles, train=self.train_smiles, k=len(gen_smiles))
+                scores_nspdk = eval_graph_list(self.test_graphs, mols_to_nx(mols), methods=['nspdk'])['nspdk']
+                metrics_dict = scores
+                metrics_dict['NSPDK'] = scores_nspdk
+                metrics_dict['validity_wo_cor'] = sum(no_corrects) / num_mols
+                wandb.log(metrics_dict)
             else:
-                mmd_results = eval_graph_list(self.test_graphs, sampled_graphs[:len(self.test_graphs)], methods=methods, kernels=kernels)
-            wandb.log(mmd_results)
+                
+                valid_sampled_trees = valid_sampled_trees[:len(self.test_graphs)]
+                sampled_graphs = [adj_to_graph(tree_to_adj(tree).numpy()) 
+                                for tree in tqdm(valid_sampled_trees, "Sampling: converting tree into graph")]
+                wandb.log({"tree-validity": len(valid_sampled_trees) / len(sampled_trees)})
+                save_graph_list(self.hparams.dataset_name, self.ts, sampled_graphs, valid_string_list, string_list, org_string_list)
+                plot_dir = f'{self.hparams.dataset_name}/{self.ts}'
+                plot_graphs_list(sampled_graphs, save_dir=plot_dir)
+                wandb.log({"samples": wandb.Image(f'./samples/fig/{plot_dir}/title.png')})
+
+                # GDSS evaluation
+                methods, kernels = load_eval_settings('')
+                if len(sampled_graphs) == 0:
+                    mmd_results = {'degree': np.nan, 'orbit': np.nan, 'cluster': np.nan, 'spectral': np.nan}
+                else:
+                    mmd_results = eval_graph_list(self.test_graphs, sampled_graphs[:len(self.test_graphs)], methods=methods, kernels=kernels)
+                wandb.log(mmd_results)
 
 
 
@@ -141,7 +172,7 @@ class BaseGeneratorLightningModule(pl.LightningModule):
 
             self.model.eval()
             with torch.no_grad():
-                if self.hparams.string_type in ['group', 'bfs-deg-group']:
+                if self.hparams.string_type in ['group', 'bfs-deg-group', 'qm9', 'zinc']:
                     sequences = self.model.decode(cur_num_samples, max_len=int(self.hparams.max_len/4), device=self.device)
                 else:
                     sequences = self.model.decode(cur_num_samples, max_len=self.hparams.max_len, device=self.device)